f6e9b4c8a05dc20bbf575d66b3a87ff1d5b58238
Elijah Lopez <elijahllopezz@gmail.com>
2019-02-17 14:04:21 -0500

    merged Firefox and chrome builds

--- scripts/build.py
+++ scripts/build.py
@@ -15,24 +15,13 @@ except ImportError:
 
 if not os.path.exists('Builds'):
     os.makedirs('Builds')
-zf_firefox = zipfile.ZipFile('Builds/Github Dark Theme (Firefox).zip', 'w')
-zf_chrome = zipfile.ZipFile('Builds/Github Dark Theme (Chrome).zip', 'w')
 
-# add manifiest.json to archive
-zf_firefox.write('dist/manifest.json', 'manifest.json')
-zf_chrome.write('dist/manifest.json', 'manifest.json')
-
-# add icons to archive
-for icon in os.listdir('dist/icons'):
-        zf_firefox.write(f'dist/icons/{icon}', f'icons/{icon}')
-        zf_chrome.write(f'dist/icons/{icon}', f'icons/{icon}')
-
-# convert sass to css
-css = sass.compile(filename='src/app.scss')
-
-# add css file to archive
-zf_firefox.writestr('app/app.css', css)
-zf_chrome.writestr('app/app.css', css)
-
-zf_firefox.close()
-zf_chrome.close()
+with zipfile.ZipFile('Builds/Github Dark Theme (Firefox + Chrome).zip', 'w') as zf:
+    zf.write('dist/manifest.json', 'manifest.json')  # add manifiest.json to archive
+    # add icons to archive
+    for icon in os.listdir('dist/icons'):
+        zf.write(f'dist/icons/{icon}', f'icons/{icon}')
+    # convert sass to css
+    css = sass.compile(filename='src/app.scss')
+    # add css file to archive
+    zf.writestr('app/app.css', css)
