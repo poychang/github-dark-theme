e5085c23a01e7bd4d342e40c422f1ebfcae0e3d0
Poy Chang <poypost@gmail.com>
2019-07-23 18:08:58 +0800

    add custom domein for enterprice github user

--- src/background.ts
+++ src/background.ts
@@ -3,22 +3,43 @@ import { config, isEmpty, storage } from './libs';
 function activateGithubDarkTheme() {
     storage.sync
         .get('domainList')
-        .then(data => (isEmpty(data) ? config.defaultDomainList : (data as string[])))
-        .then(domainList => {
-            storage.sync.clear()
-            return domainList;
+        .then(data => {
+            if (isEmpty(data)) {
+                data = { domainList: config.defaultDomainList };
+                storage.sync.set(data);
+            }
+            return data.domainList as string[];
         })
-        .then(domainList => {
-            chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-                config.defaultDomainList.forEach(url => {
-                    let regex = new RegExp(url, 'g');
-
+        .then(initDomainList => {
+            console.log(initDomainList);
+            initDomainList.forEach(url => {
+                let regex = new RegExp(url, 'g');
+                chrome.tabs.getCurrent(tab => {
+                    if (!tab) return;
+                    if (!tab.url) return;
                     if (tab.url.match(regex)) {
-                        chrome.tabs.insertCSS(tabId, {
+                        chrome.tabs.insertCSS(tab.id, {
                             file: 'app/app.css',
                             runAt: 'document_start',
                         });
                     }
+                });
+            });
+            chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+                if (!tab.url) return;
+
+                storage.sync.get('domainList').then(data => {
+                    data.domainList.forEach((url: string) => {
+                        console.log(data.domainList);
+                        console.log(tab.url);
+                        let regex = new RegExp(url, 'g');
+                        if (tab.url.match(regex)) {
+                            chrome.tabs.insertCSS(tabId, {
+                                file: 'app/app.css',
+                                runAt: 'document_start',
+                            });
+                        }
+                    });
                 });
             });
         });
